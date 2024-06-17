81cb57003b2c4f802f62a47ea2aa3d98e6b44bca
Michał <michal.klepner1@o2.pl>
2024-06-17 15:52:57 +0200

    new description

---
+++ README.md
@@ -0,0 +1,10 @@
+Important! - I am not the initiator of this project. It is made as a part of the React course. With permission of the author, i use it only as a adittion to my portfolio.
+
+This is a simple application for tourists which checks actual status of packing items for journey.
+
+Project includes
+-using UseState
+-child-parents communication
+-conditionaly sorting items by description, packing, or already packed items (checkbox)
+-sorting components in different files and also importing them
+-some array methods such as map, slice etc..
---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+import Form from "./components/Form";
+import Logo from "./components/Logo";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleDeleteAll() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+        onDeleteAll={handleDeleteAll}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
